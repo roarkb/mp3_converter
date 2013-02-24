b2654fcedd760b3ced7c3ea5d561cf0a4cdb244d
Roark Brewster <rbrewster@rim.com>
2013-02-24 10:29:03 -0800

    works better with files with single quotes

--- bulk_flac2mp3.rb
+++ bulk_flac2mp3.rb
@@ -3,16 +3,19 @@
 # lame does not have native flac support 
 # so i wrote this wrapper for flac2mp3.sh
 
-File.open("uncompressed.txt", "r").each do |e|
+SONGS = "uncompressed.txt"
+
+File.open(SONGS, "r").each do |e|
   flac = e.chomp
   mp3 = flac.gsub(/(flac)$/, "mp3")
  
   puts "converting: #{flac}\nto:         #{mp3}\n\n"
  
-  system(%Q{./flac2mp3.sh 0 '#{flac}' '#{mp3}' mtime})
+  system(%Q{./flac2mp3.sh 0 "#{flac}" "#{mp3}" mtime})
 
   if File.exists?(mp3)
-    system(%Q{rm '#{flac}'})
+    puts "removing: #{flac}..."
+    system(%Q{rm "#{flac}"})
   else
     puts "skipping for now..."
   end
